d0c24cdd1f5d1936690cc7c54ea1e4dfaede22a4
hefatss <fatsurbaby@gmail.com>
2023-07-31 15:17:07 +0700

    topaz: initial build for aosp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_topaz.mk
+    $(LOCAL_DIR)/aosp_topaz.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_topaz-eng \
-    nad_topaz-userdebug \
-    nad_topaz-user
+    aosp_topaz-eng \
+    aosp_topaz-userdebug \
+    aosp_topaz-user
--- aosp_topaz.mk
+++ aosp_topaz.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Nusantara stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from topaz device.
 $(call inherit-product, device/xiaomi/topaz/device.mk)
@@ -20,10 +20,6 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USES_BLUR := true
 USE_PIXEL_CHARGING := true
 TARGET_SUPPORTS_QUICK_TAP := true
-
-# GMS
-TARGET_GAPPS_ARCH := arm64
-USE_GAPPS := true
 
 # Device identifier
 PRODUCT_NAME := nad_topaz
